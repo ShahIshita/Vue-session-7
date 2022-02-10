cee7d652a1fa44d783a147e6efb911eca8502618
ishita <ishitas654@gmail.com>
2022-02-11 00:19:41 +0530

    session-7

---
+++ vue-first-app/src/components/Friends.vue
@@ -0,0 +1,40 @@
+<template>
+
+<li>
+  <h2>{{ friend.name }}</h2>
+  <button @click="toggledet">Show Details</button>
+
+<ul v-if="detailvisible">
+  <li><strong>Phone:</strong> {{ friend.phone }} </li>
+  <li><strong>Email:</strong> {{ friend.email }} </li>
+</ul>
+</li>
+  </template>
+
+  <script>
+
+  export default {
+  
+   data() {
+     return{
+       detailvisible:false,
+       friend: {
+           
+               id: 'ishita', 
+               name: 'Ishita Shah',
+               phone: '0897 567 991', 
+               email: 'ishita@gmail.com' 
+               },
+       
+          };
+      },
+     methods: {
+       toggledet() {
+         this.detailvisible = !this.detailvisible;
+       }
+     } 
+  };
+  </script>
+  
+    
+  
---
+++ vue-first-app/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue';
+import Friend from './components/Friend.vue';
+
+
+const app = createApp(App);
+app.component('friend', Friend);
+
+app.mount('#app')
